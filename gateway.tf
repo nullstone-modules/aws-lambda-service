0657225837fe46cd34749a6e9115a4d8581c36d7
Brad Sickles <brad.sickles@gmail.com>
2021-04-30 17:30:28 -0400

    Added vanity dns record pointing to gateway, allowing lambda invoke

--- gateway.tf
+++ gateway.tf
@@ -28,3 +28,27 @@ resource "aws_apigatewayv2_api_mapping" "this" {
 
   count = local.has_subdomain ? 1 : 0
 }
+
+resource "aws_route53_record" "vanity" {
+  name    = aws_apigatewayv2_domain_name.this[count.index].domain_name
+  type    = "A"
+  zone_id = data.ns_connection.subdomain.outputs.zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.this[count.index].domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.this[count.index].domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  count = local.has_subdomain ? 1 : 0
+}
+
+resource "aws_lambda_permission" "gateway" {
+  statement_id_prefix = "AllowGatewayAccess"
+  action              = "lambda:InvokeFunction"
+  function_name       = aws_lambda_function.this.function_name
+  principal           = "apigateway.amazonaws.com"
+  source_arn          = "${aws_apigatewayv2_api.this[count.index].execution_arn}/*/$default"
+
+  count = local.has_subdomain ? 1 : 0
+}
