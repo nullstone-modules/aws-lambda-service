b8267f52610e85e11aabf3ff5f7c063472cee138
Brad Sickles <brad.sickles@gmail.com>
2021-06-09 12:10:35 -0400

    Add log reader user to lamba service

--- logs.tf
+++ logs.tf
@@ -1,4 +1,7 @@
-resource "aws_cloudwatch_log_group" "this" {
-  name = local.resource_name
-  tags = data.ns_workspace.this.tags
+module "logs" {
+  source = "nullstone-modules/logs/aws"
+
+  name              = local.resource_name
+  tags              = data.ns_workspace.this.tags
+  enable_log_reader = true
 }
