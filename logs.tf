e54328431826ed01c3acbcc74036e3dd0b27f7ae
Brad Sickles <brad.sickles@gmail.com>
2021-06-10 08:50:12 -0400

    Fix lambda log name

--- logs.tf
+++ logs.tf
@@ -1,7 +1,8 @@
 module "logs" {
   source = "nullstone-modules/logs/aws"
 
-  name              = local.resource_name
+  // NOTE: This name must be `/aws/lambda/{function_name}` to properly collect logs from function
+  name              = "/aws/lambda/${local.resource_name}"
   tags              = data.ns_workspace.this.tags
   enable_log_reader = true
 }
