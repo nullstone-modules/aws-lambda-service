379773084d45470a375f9281c265de1c949a94fe
Brad Sickles <brad.sickles@gmail.com>
2021-11-02 14:30:30 -0400

    fmt

--- capabilities.tf
+++ capabilities.tf
@@ -30,8 +30,8 @@ locals {
       {
         logDriver = "awslogs"
         options = {
-          "awslogs-region"       = data.aws_region.this.name
-          "awslogs-group"        = module.logs.name
+          "awslogs-region"        = data.aws_region.this.name
+          "awslogs-group"         = module.logs.name
           "awslogs-stream-prefix" = local.block_name
         }
       }
