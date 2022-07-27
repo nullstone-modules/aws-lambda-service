57ada742bec5050f7fe313cc5f5bf61df5d6cf76
Brad Sickles <brad.sickles@gmail.com>
2022-07-27 15:49:58 -0400

    Fix cycle, add _SECRET_ID suffix

--- secrets.tf
+++ secrets.tf
@@ -9,7 +9,7 @@ locals {
 
   // Since lambda does not have secret injection, we are going to add a list of env vars mapping the secret ids
   // e.g. POSTGRES_URL => POSTGRES_URL_SECRET_ID = <secret-id>
-  app_secret_ids = { for key in local.secret_keys : key => aws_secretsmanager_secret.app_secret[key].id }
+  app_secret_ids = { for key in local.secret_keys : "${key}_SECRET_ID" => aws_secretsmanager_secret.app_secret[key].id }
 }
 
 resource "aws_secretsmanager_secret" "app_secret" {
