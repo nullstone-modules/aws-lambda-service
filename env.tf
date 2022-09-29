ea6f04f0c2d95ffd133c364dfb34cb2ff9a16c31
Brad Sickles <brad.sickles@gmail.com>
2022-09-29 13:49:21 -0400

    Fix ordering of env vars

--- env.tf
+++ env.tf
@@ -2,5 +2,5 @@ locals {
   standard_env_vars = tomap({
     NULLSTONE_ENV = data.ns_workspace.this.env_name
   })
-  env_vars = merge(local.cap_env_vars, local.app_secret_ids, var.service_env_vars, local.standard_env_vars)
+  env_vars = merge(local.standard_env_vars, local.cap_env_vars, local.app_secret_ids, var.service_env_vars)
 }
