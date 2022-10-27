252bc8029a39d3d93cb978fd97bf9e7bfc25b318
Brad Sickles <brad.sickles@gmail.com>
2022-10-27 14:07:34 -0400

    Fixed inability to plan secrets

--- secrets.tf
+++ secrets.tf
@@ -4,7 +4,7 @@ locals {
   //   terraform would complain about not knowing count of the resource until after apply
   // This is because the name of secrets isn't computed in the modules; only the secret value
   all_secrets = merge(local.cap_secrets, var.service_secrets)
-  secret_keys = can(nonsensitive(keys(local.all_secrets))) ? toset(nonsensitive(keys(local.all_secrets))) : toset(keys(local.all_secrets))
+  secret_keys = toset(concat(local.cap_secret_keys, nonsensitive(keys(var.service_secrets))))
 
   // Since lambda does not have secret injection, we are going to add a list of env vars mapping the secret ids
   // e.g. POSTGRES_URL => POSTGRES_URL_SECRET_ID = <secret-id>
