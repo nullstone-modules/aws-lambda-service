f581cdfd3cf30652f665754fb8176c4101292a84
Scott Sickles <scottsickles@gmail.com>
2023-08-01 14:09:55 -0400

    set the ownership controls so that the acl can be created

--- artifacts.tf
+++ artifacts.tf
@@ -10,8 +10,18 @@ resource "aws_s3_bucket" "artifacts" {
 }
 
 resource "aws_s3_bucket_acl" "artifacts" {
+  depends_on = [aws_s3_bucket_ownership_controls.default]
+
   bucket = aws_s3_bucket.artifacts.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_ownership_controls" "default" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "artifacts" {
