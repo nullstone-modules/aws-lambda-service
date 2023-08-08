7bfc54830b555312a83a0f4327a176366267af73
Brad Sickles <brad.sickles@gmail.com>
2023-08-08 09:40:58 -0400

    Fix compliance issues

--- artifacts.tf
+++ artifacts.tf
@@ -3,10 +3,38 @@ locals {
 }
 
 resource "aws_s3_bucket" "artifacts" {
+  #bridgecrew:skip=CKV_AWS_144: "Ensure that S3 bucket has cross-region replication enabled". These artifacts are only used by lambda.
+  #bridgecrew:skip=CKV_AWS_18: "Ensure the S3 bucket has access logging enabled".
   bucket              = local.artifacts_bucket_name
   tags                = local.tags
   force_destroy       = true
   object_lock_enabled = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    id     = "noop"
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_acl" "artifacts" {
