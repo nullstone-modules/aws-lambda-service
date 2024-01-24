057f2fef74948faf0f03f5503c0154c184b0c3e8
Brad Sickles <brad.sickles@gmail.com>
2024-01-24 18:22:56 -0500

    Enabled `metrics_reader` to access Cloudwatch metrics.

--- logs.tf
+++ logs.tf
@@ -4,7 +4,8 @@ module "logs" {
   version = "~>0.1.0"
 
   // NOTE: This name must be `/aws/lambda/{function_name}` to properly collect logs from function
-  name              = "/aws/lambda/${local.resource_name}"
-  tags              = local.tags
-  enable_log_reader = true
+  name               = "/aws/lambda/${local.resource_name}"
+  tags               = local.tags
+  enable_log_reader  = true
+  enable_get_metrics = true
 }
